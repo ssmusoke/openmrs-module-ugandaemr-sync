113c12308ca936bc8a44369271cc71886414c655
carapai <olupotcharles@gmail.com>
2017-03-30 09:24:20 +0300

    Updated sync module to export fingerprint

--- api/src/test/java/org/openmrs/module/ugandaemrsync/dao/UgandaEMRSyncDaoTest.java
+++ api/src/test/java/org/openmrs/module/ugandaemrsync/dao/UgandaEMRSyncDaoTest.java
@@ -7,13 +7,14 @@
  * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
  * graphic logo is a trademark of OpenMRS Inc.
  */
-package org.openmrs.module.ugandaemrsync.api.dao;
+package org.openmrs.module.ugandaemrsync.dao;
 
 import org.junit.Test;
 import org.junit.Ignore;
 import org.openmrs.api.UserService;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.ugandaemrsync.Item;
+import org.openmrs.module.ugandaemrsync.api.dao.UgandaEMRSyncDao;
 import org.openmrs.test.BaseModuleContextSensitiveTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import static org.hamcrest.Matchers.*;
